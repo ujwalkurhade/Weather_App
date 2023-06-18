37bd019d5056c54cc4c6708a9e5d72ba78905e4c
Ujwal <ujwalkurhade05@gmail.com>
2023-06-18 21:22:06 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import Shower from "./component/Shower";
 import Home from ".//component/Home";
+import Search from "./component/Search";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="Hero">
-      <Shower />
+      <Search />
+      {/* <Shower /> */}
       <Home />
     </div>
   );
