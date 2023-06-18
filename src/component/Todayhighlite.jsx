5f9a18cac65e2efeede1806038692c9c74fb45dd
Ujwal <ujwalkurhade05@gmail.com>
2023-06-18 21:15:28 +0530

    commit1

--- src/component/Todayhighlite.jsx
+++ src/component/Todayhighlite.jsx
@@ -5,10 +5,24 @@ function Todayhighlite() {
     <div>
       <h2>Today HighLights</h2>
       <div className="highlight">
-        <div className="highlightCard">hi</div>
-        <div className="highlightCard">dn</div>
-        <div className="highlightCard">dnjd</div>
-        <div className="highlightCard">dsd</div>
+        <div className="highlightCard">
+          <h3>Wind staus</h3>
+          <p>7 mph</p>
+          <h4>WSW</h4>
+        </div>
+        <div className="highlightCard">
+          <h3>Humidity</h3>
+          <p>84 %</p>
+          <h4>WSW</h4>
+        </div>
+        <div className="highlightCard">
+          <h3>Visibility</h3>
+          <p>6,4 miles</p>
+        </div>
+        <div className="highlightCard">
+          <h3>Air Pressure</h3>
+          <p>998 mb</p>
+        </div>
       </div>
     </div>
   );
